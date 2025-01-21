c6efa9d693078b7a1d72c84522268378bcc85ff1
Edson Galan <edd.galan@hotmail.com>
2025-01-21 11:46:14 -0600

    Queries with arguments - Add arguments in hello query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,9 @@
 const Query = {
-    hello: (parent, args, ctx, info) => 'Hello World!',
+    hello: (parent, args, ctx, info) => {
+        const { name } = args;
+
+        return `Hello ${name || 'World!'}`;
+    },
     quantity: () => 100,
     getUser: () => {
          return { name: 'Edson', email: 'edson@domain.com' };
