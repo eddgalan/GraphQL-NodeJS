9180f6309964f6029f22d36e00c8ed0a4cf6b2bb
Edson Galan <edd.galan@hotmail.com>
2025-01-20 19:26:53 -0600

    Defines the queries 'quantity' and 'getUser' - Defines the 'getUser' Query of type 'User' - Defines the 'User' Query - Add 'getUser' Resolver - Query:     {       hello,       quantity,       getUser {         name,         email       }     }

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,9 @@
 const Query = {
     hello: (parent, args, ctx, info) => 'Hello World!',
+    quantity: () => 100,
+    getUser: () => {
+         return { name: 'Edson', email: 'edson@domain.com' };
+    },
 };
 
 export default Query;
